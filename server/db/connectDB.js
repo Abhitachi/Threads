cdc5accbb9158c7138d8b96c1c2718a7d824a9ed
Abhitachi <abhishekshetter1999@gmail.com>
2024-06-25 19:00:42 +0530

    updated

--- server/db/connectDB.js
+++ server/db/connectDB.js
@@ -1,8 +1,20 @@
 import mongoose from "mongoose";
+import dotenv from 'dotenv'
+
+
+
+
+dotenv.config();
+
 
 const connectDB = async () => {
+ console.log(process.env.MONGO_URI, ' uri');
   try {
-    const connect = await mongoose.connect(process.env.MONGO_URI);
+    const connect = await mongoose.connect(process.env.MONGO_URI || "mongodb+srv://abhishekshetter1999:c9Cvmq7oGhnHFk5l@threads.nknntrm.mongodb.net/threads?retryWrites=true&w=majority&appName=threads",{
+      //must add in order to not get any error masseges:
+      useUnifiedTopology:true,
+      useNewUrlParser: true,
+  });
     console.log(`Mongo connected`);
   } catch (error) {
     console.log(`error: ${error.message}`);
